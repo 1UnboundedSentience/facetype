9b2cd1b160c978c3e1847bbd49a3a77e89fa22b6
Andy Jiang <andyjiang@gmail.com>
2015-05-11 20:10:45 -0700

    updated server/lib/routes.js

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -4,8 +4,6 @@
  */
 
 var render = require('./render');
-var User = require('./user');
-var rank = require('./elo');
 
 /**
  * Render index html page.
@@ -13,38 +11,4 @@ var rank = require('./elo');
 
 exports.index = function *() {
   return this.body = yield render('index');
-};
-
-/**
- * List users.
- */
-
-exports.list = function *() {
-  return this.body = yield User.find({});
-};
-
-/**
- * Create user
- */
-
-exports.create = function *() {
-  var load = this.request.body;
-  return this.body = yield User.create(load.player);
-};
-
-/**
- * Delete user
- */
-
-exports.remove = function *(name) {
-  return this.body = yield User.remove({ name: name });
-};
-
-/**
- * Submit results.
- */
-
-exports.results = function *() {
-  var load = this.request.body;
-  return this.body = yield rank(load.winner, load.loser);
 };
